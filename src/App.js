5e4cd3a685bbbbc7d0e2daddfa7572f457899abc
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-01-10 16:39:44 -0500

    adding-input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import "./App.css";
-import './key'
+import './key';
+import axios from "axios";
+import react,{useState} from "react";
 
-function App(props) {
-  let url = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=$props.api_id}&app_key=${props.api_key}&from=0&to=36&calories=591-722&health=alcohol-free`;
+function App() {
+  const [query,setQuery]=useState("")
+  const api_id="161c446a"
+  const api_key="dcf65627400407ddfa85671bd611aa83"
+
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${api_id}&app_key=${api_key}&from=0&to=36&calories=591-722&health=alcohol-free`;
+
+  function getRecipies(){
+    axios
+    .get(`${url}`)
+    .then((result) => {
+      console.log(result);
+     })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
   return (
+
     <div className="App">
-      <h1>Hello React</h1>
+      <h1 onClick={getRecipies}>Food Recipe App</h1>
+      <form className="app_searchForm">
+        <input type="text" placeholder="enter ingridient" value={query} onChange={(e)=>setQuery(e.target.value)}></input>
+      </form>
     </div>
   );
 }

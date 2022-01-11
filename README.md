cf150b04a6caf1ed4731cbce1ccf9bf770eecf2b
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-01-10 22:49:04 -0500

    modifying read.ME

--- README.md
+++ README.md
@@ -68,3 +68,50 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+## Key Learnings
+
+- Fully understood and implemented React fundamentals, react hook.
+- Implemented the basics of Node.js.
+- Used Semantic HTML.
+- Flexbox, SASS.
+
+
+## Installation
+
+### Clone
+
+- Clone this repo to your local machine using `https://github.com/dennisagarwal/expense-application.git`
+
+### Setup
+
+ **1.** Navigate to the express-application folder in your terminal and install npm like this:
+
+```
+$ npm install
+```
+### Launch application
+
+```
+$ npm start
+```
+
+## Tech Stack
+
+- React JS.
+- React Hook.
+- Node.js
+- APIs.
+- HTML.
+- Flexbox, SASS.
+
+
+#### Website Glimpse
+
+ **1.** Landing Page
+
+ ![](images/landing-page.JPG)
+
+  ##### Website Link
+
+ https://hungry-hodgkin-037c05.netlify.app/
--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import RecipeTile from "./Components/RecipeTile/RecipeTile";
 function App() {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
-
+const [healthLabels, setHealthLabels]=useState("vegan")
   const api_id = "161c446a";
   const api_key = "dcf65627400407ddfa85671bd611aa83";
 
-  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${api_id}&app_key=${api_key}&calories=591-722&health=alcohol-free`;
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${api_id}&app_key=${api_key}&health=${healthLabels}`;
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -41,8 +41,23 @@ function App() {
           onChange={(e) => setQuery(e.target.value)}
         ></input>
         <input className="app__submit" type="submit" value="Search"></input>
+        <select className="app__healthLabels">
+        <option value="vegan" onClick={()=>setHealthLabels("vegan")}>Vegan</option>
+        <option value="vegetarian" onClick={()=>setHealthLabels("vegetarian")}>Vegetarian</option>
+        <option value-="paleo" onClick={()=>setHealthLabels("paleo")}>Paleo</option>
+        <option value-="dairy-free" onClick={()=>setHealthLabels("dairy-free")}>Dairy Free</option>
+        <option value-="wheat-free" onClick={()=>setHealthLabels("wheat-free")}>Wheat Free</option>
+        <option value-="gluten-free" onClick={()=>setHealthLabels("gluten-free")}>Gluten Free</option>
+        <option value-="low-sugar" onClick={()=>setHealthLabels("low-sugar")}>Low Sugar</option>
+        <option value-="peanut-free" onClick={()=>setHealthLabels("peanut-free")}>Peanut Free</option>
+        <option value-="egg-free" onClick={()=>setHealthLabels("Egg-free")}>Egg Free</option>
+        <option value-="tree-nut-free" onClick={()=>setHealthLabels("tree-nut-free")}>Tree Nut Free</option>
+        <option value-="soy-free" onClick={()=>setHealthLabels("soy-free")}>Soy Free</option>
+        <option value-="fish-free" onClick={()=>setHealthLabels("fish-free")}>Fish Free</option>
+        <option value-="shellfish-free" onClick={()=>setHealthLabels("shellfish-free")}>ShellFish Free</option>
+        </select>
       </form>
-      <div>
+      <div className="app__recipes">
         {recipes.map((recipe) => {
           return <RecipeTile recipe={recipe}></RecipeTile>
           //green recipe is from recipeTile

c3a4b4e9c32aeddfd0979e90edd21a802fdca37f
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-01-10 22:09:59 -0500

    adding-images

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
 import "./App.css";
-import './key';
+import "./key";
 import axios from "axios";
-import react,{useState} from "react";
-
+import react, { useState } from "react";
+import RecipeTile from "./Components/RecipeTile/RecipeTile";
 function App() {
-  const [query,setQuery]=useState("")
-  const api_id="161c446a"
-  const api_key="dcf65627400407ddfa85671bd611aa83"
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
 
-  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${api_id}&app_key=${api_key}&from=0&to=36&calories=591-722&health=alcohol-free`;
+  const api_id = "161c446a";
+  const api_key = "dcf65627400407ddfa85671bd611aa83";
 
-  function getRecipies(){
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${api_id}&app_key=${api_key}&calories=591-722&health=alcohol-free`;
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    getRecipies();
+  };
+
+  function getRecipies() {
     axios
-    .get(`${url}`)
-    .then((result) => {
-      console.log(result);
-     })
-    .catch((error) => {
-      console.log(error);
-    });
+      .get(`${url}`)
+      .then((result) => {
+        console.log(result.data);
+        setRecipes(result.data.hits);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
-
-    <div className="App">
-      <h1 onClick={getRecipies}>Food Recipe App</h1>
-      <form className="app_searchForm">
-        <input type="text" placeholder="enter ingridient" value={query} onChange={(e)=>setQuery(e.target.value)}></input>
+    <div className="app">
+      <h1>Food Recipe App</h1>
+      <form className="app__searchForm" onSubmit={submitHandler}>
+        <input
+          className="app__input"
+          type="text"
+          placeholder="enter ingridient"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        ></input>
+        <input className="app__submit" type="submit" value="Search"></input>
       </form>
+      <div>
+        {recipes.map((recipe) => {
+          return <RecipeTile recipe={recipe}></RecipeTile>
+          //green recipe is from recipeTile
+          //blue recipe is arguement
+            {/* return <p>{recipe["recipe"]["label"]}</p>; */}
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/Components/RecipeTile/RecipeTile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./RecipeTile.css"
+function RecipeTile({recipe}) {
+  return (
+    <div className='recipeTile'>
+    <img className='recipeTile__img' src={recipe["recipe"]["image"]}></img>
+ <p className='recipeTile__name'>{recipe.recipe.label}</p>
+    </div>
+  )
+}
+
+export default RecipeTile
